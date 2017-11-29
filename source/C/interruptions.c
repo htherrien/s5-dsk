451df693f6b329a491fe1d1bd9ea6dd7ced62c23
roim1901 <roim1901@LAB3024-GEL18.usherbrooke.ca>
2017-11-29 17:30:55 -0500

    Moved the interruptions from the main to its own file.

---
+++ source/C/interruptions.c
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Projet S5
+ * @file    interruptions.c
+ * @author  Hugo Therrien, Hugo Daniel, Louis D'Amour, Marc-André Roireau
+ * @date    29 novembre 2017
+ * @version 0.1
+ *
+ * Contient les interruptions
+ *
+ ******************************************************************************/
+
+#include <DSK6713_led.h>
+#include <csl_mcbsp.h>
+#include <DSK6713.h>
+
+#include "signaux.h"
+#include "acquisitionSignal.h"
+#include "correlations3Axes.h"
+#include "faireFFT.h"
+#include "SPI_driver.h"
+
+// FFT (daml2601)
+static float signalAFFT[2*TAILLE_FFT];
+#pragma DATA_ALIGN(signalAFFT, 8)
+
+static Signal3Axes signalACorreler; // Déja aligné.
+Signal3AxesReference signalReference;
+
+interrupt void intTimer0(void)
+{
+    static Signal3AxesPtr signalACorrelerPtr = {signalACorreler.x, signalACorreler.y, signalACorreler.z};
+    int resultat;
+    acquistionCorrelationDemo(&signalACorrelerPtr);
+    resultat = correler3Axes(&signalACorrelerPtr, &signalReference);
+
+    // FFT (daml2601)
+    int resultat_fft = 0;
+    static int i = 0;
+    acquistionCorrelationDemoFFT(&signalAFFT[i]);
+    i = i + 2;
+    if (i == 2*TAILLE_FFT)
+    {
+        resultat_fft = faireFFT(signalAFFT);
+        i = 0;
+    }
+
+    if(resultat)
+    {
+        DSK6713_LED_on(0);
+    }
+    else
+    {
+        DSK6713_LED_off(0);
+    }
+    if(resultat_fft)
+    {
+        DSK6713_LED_on(3);
+    }
+    else
+    {
+        DSK6713_LED_off(3);
+    }
+}
+
+/*
+ * When interruption on the pin4:
+ * 1   Read the data byte on the SPI bus.
+ */
+interrupt void c_int04(void)
+{
+    static int UARTData;
+    extern MCBSP_Handle MCBSP0Handle;
+    MCBSP_write(MCBSP0Handle, SPI_READ_DATA);
+    DSK6713_waitusec(10);
+    UARTData = (MCBSP_read(MCBSP0Handle) & 0x00FF);
+    if(0x34 == UARTData)
+    {
+        int a = 1;
+    }
+}
